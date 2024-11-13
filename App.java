bc69881bdbe78d32d1b8abeabfba8dfd1100e22f
JunChoi <chldnjswns1@daum.net>
2024-11-13 16:25:00 +0900

    :hammer:refactor: operator input point

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 // 추후 학습 내용이 더해지면 연산자, num1, num2 입력을 제대로 처리할 것
 // 지금은 연산자, num1, num2가 제대로 입력된다고 가정하며
-// num1과 num2는 int가 아니면 그냥 프로그램이 터지도록 처리.
+// 잘못된 값이 입력되면 그 즉시 프로그램이 터지도록 기획
 
 
 public class App {
@@ -15,8 +15,18 @@ public class App {
             System.out.println("--------------------");
             System.out.println("새로운 연산을 시작합니다");
 
+            String[] operators = {"+", "-", "*", "/"};
             System.out.println("연산자를 입력해 주세요.(+, -, *, /)");
-            String op = sc.nextLine();
+            String operator = sc.nextLine();
+            for (String op : operators) {
+                if (operator.equals(op)){
+                    break;
+                } else {
+                    System.out.println("잘못된 연산자 입력");
+                    return;
+                }
+
+            }
 
             System.out.println("num1을 입력해주세요");
             String tmp1 = sc.nextLine();
@@ -26,7 +36,7 @@ public class App {
             String tmp2 = sc.nextLine();
             int num2 = Integer.parseUnsignedInt(tmp2);
 
-            switch (op) {
+            switch (operator) {
                 case "+":
                     System.out.println(num1 + num2);
                     break;
@@ -44,7 +54,7 @@ public class App {
                     System.out.println(num1 / num2);
                     break;
                 default:
-                    System.out.println("비정상적인 연산자를 입력했습니다.");
+                    assert (false) : "unknown case";
                     break;
             }
 
