6b1a4232577872f1a48478af5d6fc97d81228e24
JunChoi <chldnjswns1@daum.net>
2024-11-13 15:52:28 +0900

    계산기1 완성.(App.java)

--- App.java
+++ App.java
@@ -1,35 +1,67 @@
 import java.util.Scanner;
 
 
+// 추후 학습 내용이 더해지면 연산자, num1, num2 입력을 제대로 처리할 것
+// 지금은 연산자, num1, num2가 제대로 입력된다고 가정하며
+// num1과 num2는 int가 아니면 그냥 프로그램이 터지도록 처리.
+
+
 public class App
 {
-    enum Operation { PLUS, MINUS, MULTIPLY, DIVIDE }
 
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("연산자를 입력해 주세요.");
-        sc.nextLine();
 
+        while(true)
+        {
+            System.out.println("--------------------");
+            System.out.println("새로운 연산을 시작합니다");
 
+            System.out.println("연산자를 입력해 주세요.(+, -, *, /)");
+            String op = sc.nextLine();
 
+            System.out.println("num1을 입력해주세요");
+            String tmp1 = sc.nextLine();
+            int num1 = Integer.parseUnsignedInt(tmp1);
 
-        switch ()
-        // input흐름 제어
-        // output GUI
-        //
-//
-//
-    }
+            System.out.println("num2를 입력해주세요");
+            String tmp2 = sc.nextLine();
+            int num2 = Integer.parseUnsignedInt(tmp2);
 
-    public static boolean isValidOperation(String input)
-    {
-        try{
-            Operation.valueOf(input.toUpperCase());
-            return true;
+            switch (op)
+            {
+                case "+":
+                    System.out.println(num1 + num2);
+                    break;
+                case "-":
+                    System.out.println(num1 - num2);
+                    break;
+                case "*":
+                    System.out.println(num1 * num2);
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        break;
+                    }
+                    System.out.println(num1 / num2);
+                    break;
+                default:
+                    System.out.println("비정상적인 연산자를 입력했습니다.");
+                    break;
+            }
+
+            System.out.println("연산 종료 실행? (exit를 입력하면 종료. 그 외의 경우 반복)");
+            String exit = sc.nextLine();
+            if(exit.equals("exit"))
+            {
+                System.out.println("종료합니다.");
+                break;
+            }
         }
-        catch (IllegalArgumentException e){
-            return false;
-        }
+
+        sc.close();
+
     }
 }
