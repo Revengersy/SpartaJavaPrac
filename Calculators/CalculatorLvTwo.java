16fab1d528c0274b19dd94c73b63c8cef2e209ce
JunChoi <chldnjswns1@daum.net>
2024-11-14 19:48:20 +0900

    :sparkles:feat: basic calculations

---
+++ Calculators/CalculatorLvTwo.java
@@ -0,0 +1,20 @@
+package Calculators;
+
+public class CalculatorLvTwo {
+
+    public double addTwoNumbers(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtractTwoNumbers(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiplyTwoNumbers(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public double divideTwoNumbers(double num1, double num2){
+        return num1 / num2;
+    }
+}
